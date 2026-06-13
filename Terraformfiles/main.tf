acd90833ddbf22d5b86ac3ea133bfe8674a61909
shuchichawda <shuchi.chawda@gmail.com>
2026-06-13 11:08:51 +0000

    tf files added

---
+++ Terraformfiles/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "shuchi-rg-cicd-ntms-workshop"
+  location = "East US"
+}
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "shuchi-vnet-cicd-workshop"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+
+resource "azurerm_subnet" "subnet" {
+  name = "web-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name = azurerm_resource_group.rg.name
+}
