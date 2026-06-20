81325d14a5e7c47463589eb860132e7e828fe101
ShuchiChawda@ <shuchi.chawda@gmail.com>
2026-06-20 10:01:06 +0000

    added

--- Terraformfiles/main.tf
+++ Terraformfiles/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "shuchi-vnet-cicd-workshop"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
 }
 
 
