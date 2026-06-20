a43339d22757da3f99afe56301cf19636becff66
ShuchiChawda@ <shuchi.chawda@gmail.com>
2026-06-20 11:41:34 +0000

    App subnet created

--- Terraformfiles/main.tf
+++ Terraformfiles/main.tf
@@ -24,3 +24,4 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
   address_prefixes     = ["10.1.1.0/24"]
+}
